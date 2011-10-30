c22209f97efa0e7ef261ffae24232ed9e1fb482c
Lathanael <101@abwesend.de>
2011-10-30 22:57:42 +0100

    Spout InputListener

---
+++ src/main/java/de/Lathanael/TheLivingForce/Listeners/TLFInputListener.java
@@ -0,0 +1,11 @@
+package de.Lathanael.TheLivingForce.Listeners;
+
+import org.getspout.spoutapi.event.input.InputListener;
+import org.getspout.spoutapi.event.input.KeyPressedEvent;
+
+public class TLFInputListener extends InputListener {
+
+	@Override
+	public void onKeyPressedEvent(KeyPressedEvent event) {
+	}
+}
