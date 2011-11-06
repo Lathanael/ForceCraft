efca801310c65916a20ff7869d42c250169a6713
Lathanael <101@abwesend.de>
2011-11-06 21:23:46 +0100

    Started working on the Key system. Key pressed -> execute power on target!

--- src/main/java/de/Lathanael/ForceCraft/Listeners/TLFInputListener.java
+++ src/main/java/de/Lathanael/ForceCraft/Listeners/TLFInputListener.java
@@ -20,23 +20,38 @@
 
 package de.Lathanael.ForceCraft.Listeners;
 
+import org.bukkit.command.Command;
 import org.getspout.spoutapi.event.input.InputListener;
 import org.getspout.spoutapi.event.input.KeyPressedEvent;
+import org.getspout.spoutapi.player.SpoutPlayer;
 
 import de.Lathanael.ForceCraft.Players.ForcePlayer;
 import de.Lathanael.ForceCraft.Players.PlayerHandler;
+import de.Lathanael.ForceCraft.Powers.BasePower;
+import de.Lathanael.ForceCraft.bukkit.ForcePlugin;
 
 /**
  * @author Lathanael (aka Philippe Leipold)
  */
 public class TLFInputListener extends InputListener {
+	protected final ForcePlugin instance;
+
+	public TLFInputListener(ForcePlugin instance) {
+		this.instance = instance;
+	}
 
 	@Override
 	public void onKeyPressedEvent(KeyPressedEvent event) {
-		ForcePlayer player = PlayerHandler.getInstance().getPlayer(event.getPlayer().getName());
-		if (player == null)
+		ForcePlayer fPlayer = PlayerHandler.getInstance().getPlayer(event.getPlayer().getName());
+		SpoutPlayer sPlayer = event.getPlayer();
+		if (fPlayer == null)
 			return;
-		if (!player.containsKey(event.getKey()))
+		if (!fPlayer.containsKey(event.getKey()))
 			return;
+		Command cmd = instance.getCommand(fPlayer.getKey(event.getKey()));
+		sPlayer.getTargetBlock(null, 600);
+		BasePower power = instance.commandsHandler.getCmdPower(cmd);
+		if (power != null)
+			instance.commandsHandler.executePower(sPlayer.getPlayer(), power, null);
 	}
 }
