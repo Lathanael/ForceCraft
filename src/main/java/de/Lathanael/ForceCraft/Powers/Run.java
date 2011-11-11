5e47be251e521ef6262b7d5f06fedd7c68b6fa1f
Lathanael <101@abwesend.de>
2011-11-11 22:22:16 +0100

    Powers..

---
+++ src/main/java/de/Lathanael/ForceCraft/Powers/Run.java
@@ -0,0 +1,60 @@
+/*************************************************************************
+ * Copyright (C) 2011  Philippe Leipold
+ *
+ * This file is part of TheLivingForce.
+ *
+ * TheLivingForce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TheLivingForce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TheLivingForce. If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+package de.Lathanael.ForceCraft.Powers;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.Lathanael.ForceCraft.Commands.PermissionsHandler;
+import de.Lathanael.ForceCraft.Players.ForcePlayer;
+import de.Lathanael.ForceCraft.Utils.ForceAlignment;
+import de.Lathanael.ForceCraft.Utils.PlayerPowerStates;
+import de.Lathanael.ForceCraft.Utils.Tools;
+
+/**
+ * @author Lathanael (aka Philippe Leipold)
+ */
+public class Run extends BasePower {
+
+	public Run() {
+		name = "Run";
+		cmdName = "tlf_run";
+		perm = "force.run";
+		alignment = ForceAlignment.valueOf(instance.config.getString("Power." + name + ".alignment"));
+		rank = instance.config.getInt("Power." + name + ".rank");
+		delay = instance.config.getLong("Power." + name + ".delay");
+		manaCost = instance.config.getInt("Power." + name + ".mana");
+	}
+
+	@Override
+	public void execute(ForcePlayer player) {
+		player.setPowerState(PlayerPowerStates.RUN);
+	}
+
+	@Override
+	public boolean checkPerm(CommandSender sender) {
+		if (!Tools.isPlayer(sender, true))
+			return false;
+		else if (PermissionsHandler.getInstance().hasPerm((Player) sender, perm))
+			return true;
+		return false;
+	}
+}
