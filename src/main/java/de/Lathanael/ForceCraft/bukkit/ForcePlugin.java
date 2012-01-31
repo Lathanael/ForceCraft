1648529295b88b7cc715ae75cfb638e8ceafc204
Lathanael <101@abwesend.de>
2012-01-31 14:50:08 +0100

    Corrected License text a little bit

--- src/main/java/de/Lathanael/ForceCraft/bukkit/ForcePlugin.java
+++ src/main/java/de/Lathanael/ForceCraft/bukkit/ForcePlugin.java
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (C) 2011-2012  Philippe Leipold
+ * Copyright (C) 2011-2012 Philippe Leipold
  *
  * ForceCraft is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
